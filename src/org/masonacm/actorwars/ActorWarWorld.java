9803e32284096b5919e52e24ec2a1a25f3b11a39
Schuyler Cebulskie <Gawdl3y@gmail.com>
2013-05-17 14:04:20 -0400

    Rewrote the ActorWarWorld constructor

--- src/org/masonacm/actorwars/ActorWarWorld.java
+++ src/org/masonacm/actorwars/ActorWarWorld.java
@@ -4,30 +4,18 @@ import info.gridworld.actor.Actor;
 import info.gridworld.actor.ActorWorld;
 import info.gridworld.grid.BoundedGrid;
 
+/**
+ * The world for ActorWars
+ */
 public class ActorWarWorld extends ActorWorld {
     public ActorWarWorld() {
         setGrid(new BoundedGrid<Actor>(42, 42));
-        int i = 0;
-        while(i < 40) {
-            add(new Tree());
-            i++;
-        }
-        i = 0;
-        while(i < 10) {
-            add(new Wheat());
-            i++;
-        }
-        i = 0;
-        while(i < 10) {
-            add(new Rock());
-            i++;
-        }
-        i = 0;
-        while(i < 5) {
-            add(new IronOre());
-            i++;
-        }
-        add(new Quarry());
-        add(new Quarry());
+
+        int i;
+        for(i = 0; i < 40; i++) add(new Tree());
+        for(i = 0; i < 10; i++) add(new Wheat());
+        for(i = 0; i < 10; i++) add(new Rock());
+        for(i = 0; i < 5; i++) add(new IronOre());
+        for(i = 0; i < 2; i++) add(new Quarry());
     }
 }
